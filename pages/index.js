edbe1d232a239e3f4a7f907e817bafe468be57a7
eric <ericwang27759@gmail.com>
2024-01-15 21:14:39 -0800

    added some components to the home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
-const HomePage = () => (
-    <div>
-        <h1>The Home Page</h1>
-    </div>
-);
+import EventList from "../components/events/event-list";
+import { getFeaturedEvents } from "../dummy-data";
+
+const HomePage = () => {
+    const featuredEvents = getFeaturedEvents();
+    console.log({featuredEvents})
+
+    return (
+        <div>
+            <EventList events={featuredEvents}/>
+        </div>
+    );
+};
 
 export default HomePage;
