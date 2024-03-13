a31996afe450c6e70c7b338e60c5d37347fb724a
eric <ericwang27759@gmail.com>
2024-03-12 22:08:29 -0700

    added core functionality of adding newsletter route

--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,12 +1,22 @@
+import { useRef } from 'react';
 import classes from './newsletter-registration.module.css';
 
 const NewsletterRegistration = () => {
+    const emailInputRef = useRef();
     const registrationHandler = (event) => {
         event.preventDefault();
 
-        // fetch user input (state or refs)
-        // optional: validate input
-        // send valid data to API
+        const enteredEmail = emailInputRef.current.value;
+
+        fetch('/api/newsletter', {
+            method: 'POST',
+            body: JSON.stringify({ email: enteredEmail }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((res) => res.json())
+        .then((data) => console.log(data));
     }
 
     return (
@@ -19,6 +29,7 @@ const NewsletterRegistration = () => {
                         id='email'
                         placeholder='Your email'
                         aria-label='Your email'
+                        ref={emailInputRef}
                     />
                     <button>Register</button>
                 </div>
---
+++ pages/api/newsletter.js
@@ -0,0 +1,14 @@
+const handler = (req, res) => {
+    if (req.method === 'POST') {
+        const { email } = req.body;
+
+        if (!email || !email.includes('@')) {
+            res.status(422).json({ message: 'Invalid email address.' });
+            return;
+        }
+
+        res.status(201).json({ message: 'Signed up!' });
+    }
+};
+
+export default handler;
