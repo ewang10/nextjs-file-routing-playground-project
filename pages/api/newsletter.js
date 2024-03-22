354f0e9c649c99a180961b38be30f14bfb9a2235
eric <ericwang27759@gmail.com>
2024-03-22 16:39:22 -0700

    setting up mongodb and integrating in newsletter api

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-const handler = (req, res) => {
+import { MongoClient } from 'mongodb';
+
+const handler = async (req, res) => {
     if (req.method === 'POST') {
         const { email } = req.body;
 
@@ -6,6 +8,13 @@ const handler = (req, res) => {
             res.status(422).json({ message: 'Invalid email address.' });
             return;
         }
+
+        const client = await MongoClient.connect('mongodb+srv://ericDB:ewh2elgoTw8jabyH@cluster0.lidjonx.mongodb.net/newsletter?retryWrites=true&w=majority&appName=Cluster0');
+        const db = client.db();
+
+        await db.collection('emails').insertOne({ email });
+
+        client.close();
 
         res.status(201).json({ message: 'Signed up!' });
     }
